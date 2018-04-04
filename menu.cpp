3ae243c55248d85544f4d7c85e5c563ee6a46359
Đào Duy Nam <lenkc98@gmail.com>
2018-04-04 20:34:06 +0700

    Update 4/4

--- menu.cpp
+++ menu.cpp
@@ -1,14 +1,13 @@
-#include<iostream>
-#include<cstdio>
-#include"startFunction.h"
-#include"menu.h"
-#include"typedef.h"
-#include"main.h"
+#include <iostream>
+#include <cstdio>
+#include "startFunction.h"
+#include "menu.h"
+#include "typedef.h"
+#include "main.h"
 using namespace std;
 
 
-
-//hàm hiển thị các chức năng cho người dùng chọn
+// hàm hiển thị các chức năng cho người dùng chọn
 void displayMenu()
 {
     cout<<"----------------------------------------------CHUONG TRINH CAN LE VAN BAN-----------------------------------------------"<<endl
@@ -22,45 +21,37 @@ void displayMenu()
 
 
 //hàm cho người dùng chọn các chức năng chính
-void selectMenu()
-{
-do{
+void selectMenu() {
+    do {
         cin.clear();
         cout<<"->Chon chuc nang(1-3):";
         cin>>selection;
         fflush(stdin);
-    }while(selection!=1&&selection!=2&&selection!=3);
+    }
+    while (selection!=1&&selection!=2&&selection!=3);
 	
-	
-	
-	//Xóa đi,không cần thiết,chỉ để test cấu trúc chương trình
-    char inputText[20];
-    inputText[0]='\0';//bằng '\0' để coi như chưa có văn bản nhập vào
-	
-	
-	
-	
-	
-if(selection==2&&inputText[0]=='\0')    //Nếu văn bản chưa được nhập mà người dùng yêu câu căn lề
-{
-    cout<<"Chua nhap van ban,hay nhap van ban truoc!"<<endl;
-    selectMenu();
-    return;
-}
-//Thực hiện chức năng theo lựa chọn người dùng
-switch (selection) {
-        case 1:
-            cout<<"Chon phuong thuc nhap van ban:"<<endl
-            	<<"     1.Nhap tu ban phim"<<endl
-            	<<"     2.Trich xuat tu file"<<endl;
-            do{
-        cin.clear();
-        cout<<"->Chon chuc nang(1-2):";
-        cin>>selection;
-        fflush(stdin);
-                }while(selection!=1&&selection!=2);
-if(selection==1) startGetInputKB(); //hàm lấy input từ keyboard
-else startGetInputFile();//hàm lấy input từ file
+    // Nếu văn bản chưa được nhập mà người dùng yêu câu căn lề
+    if (selection==2&&inputText[0]=='\0') {
+        cout<<"Chua nhap van ban,hay nhap van ban truoc!"<<endl;
+        selectMenu();
+        return;
+    }
+
+    //Thực hiện chức năng theo lựa chọn người dùng
+    switch (selection) {
+      case 1:
+        cout<<"Chon phuong thuc nhap van ban:"<<endl
+            <<"     1.Nhap tu ban phim"<<endl
+        	<<"     2.Trich xuat tu file"<<endl;
+        do {
+            cin.clear();
+            cout<<"->Chon chuc nang(1-2):";
+            cin>>selection;
+            fflush(stdin);
+        }
+        while (selection!=1&&selection!=2);
+        if(selection==1) startGetInputKB(); // hàm lấy input từ keyboard
+        else startGetInputFile(); // hàm lấy input từ file
             break;
         case 2:
         	cout<<"Chon kieu can le:"<<endl
